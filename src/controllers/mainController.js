5d6fd5fc114e2f58d8ef9aa819b2c08343144c1e
pabloferreri <pab.ferreri@gmail.com>
2021-02-13 01:00:04 -0300

    index, ejs and first commit

---
+++ src/controllers/mainController.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+
+		const productsInSale = products.filter(product => product.category == "in-sale");
+		const LastVisited = products.filter(product => product.category == "visited");	
+
+
+		res.render("index", {LastVisited: LastVisited, productsInSale: productsInSale})
+	},
+	/*search: (req, res) => {
+		// Do the magic
+	},*/
+};
+
+module.exports = controller;
